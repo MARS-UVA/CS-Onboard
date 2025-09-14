76cd0cba5a915247f37016a990fe1d677576916c
ericzn248 <ericzn248@gmail.com>
2025-09-13 20:13:00 -0400

    proving my haters wrong

--- src/realsense/src/motion_capture.cpp
+++ src/realsense/src/motion_capture.cpp
@@ -8,14 +8,13 @@
 #include <std_msgs/msg/string.hpp>
 #include <iostream>
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
 #include <librealsense2/rs.hpp>
-#include <chrono>
 #include <functional>
 #include <memory>
-#include <string>
-
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 
 using namespace::std::chrono_literals;
 
@@ -41,11 +40,10 @@ class RSNode : public rclcpp::Node {
             }
 
             try {
-                pipeline_ = std::make_unique<rs2::pipeline>();
                 rs2::config cfg;
-                // cfg.enable_stream(RS2_STREAM_DEPTH, 848, 480, RS2_FORMAT_Z16, 30);
-                cfg.enable_stream(RS2_STREAM_COLOR, 1280, 720, RS2_FORMAT_RGB8, 30);
-                pipeline_->start(cfg);
+                cfg.enable_stream(RS2_STREAM_DEPTH, 848, 480, RS2_FORMAT_Z16, 30);
+                // cfg.enable_stream(RS2_STREAM_COLOR, 1280, 720, RS2_FORMAT_RGB8, 30);
+                pipeline_.start(cfg);
             } catch (const rs2::error & e) {
                 RCLCPP_ERROR(this->get_logger(),
                             "Failed to start RealSense pipeline: %s", e.what());
@@ -53,14 +51,22 @@ class RSNode : public rclcpp::Node {
         }
 
     private:
-        std::unique_ptr<rs2::pipeline> pipeline_;
+        rs2::pipeline pipeline_;
         rclcpp::Publisher<std_msgs::msg::String>::SharedPtr example_publisher_;
         rclcpp::TimerBase::SharedPtr timer_;
         void timer_callback()
         {
-            printf("waiting for frame\n");
-            rs2::frameset frame = pipeline_->wait_for_frames();
-            printf("frame received\n");
+            rs2::frameset frames = pipeline_.wait_for_frames();
+            rs2::depth_frame dpt_frame = frames.get_depth_frame();
+            std::cout<<"wow i found a frame"<<std::endl;
+            cv::Size dpt_size = cv::Size(dpt_frame.get_width(), dpt_frame.get_height());
+            rs2::colorizer dpt_rgb(0);
+            rs2::video_frame dpt_colored = dpt_rgb.colorize(dpt_frame);
+
+            
+            cv::Mat img = cv::Mat(dpt_size, CV_8UC3, (void*)dpt_colored.get_data(), cv::Mat::AUTO_STEP);
+            cv::imshow("craig sucks at coding", img);
+            cv::waitKey(500);
             auto message = std_msgs::msg::String();
             message.data = "publishing";
             example_publisher_->publish(message);
@@ -72,8 +78,7 @@ int main(int argc, char ** argv)
     std::cout << ("Started main\n");
     rclcpp::init(argc, argv);
     std::cout << ("Initialized ROS\n");
-    // RSNode node = RSNode();
-    // auto node = rclcpp::Node("example_publisher");
+
     auto node = std::make_shared<RSNode>();
     std::cout << "Starting pipeline 0\n";
     node->startPipeline();
